a94f243f380d1091fb794873cfd5e501bb7ee1ab
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2025-05-01 22:49:07 +0900

    Add rules of wasmedge network extension for wasip1

--- rules/wasmedgesyscalls.go
+++ rules/wasmedgesyscalls.go
@@ -11,11 +11,6 @@ import (
 //go:embed wasmedgesyscalls/*
 var fs embed.FS
 
-var wasmEdgeSyscallsReplaceFiles = map[string]string{
-	"src/syscall/net_fake.go":   "net_fake.go.replaced",
-	"src/syscall/net_wasip1.go": "net_wasip1.go.replaced",
-}
-
 type WASMEdgeSyscalls struct {
 }
 
@@ -25,12 +20,34 @@ func (w *WASMEdgeSyscalls) Apply(ctx *ExecContext) error {
 		return nil
 	}
 
-	// If we couldn't determine the package path, return unmodified args
-	if ctx.Package != "syscall" {
+	var err error
+	switch ctx.Package {
+	case "syscall":
+		err = w.processSyscallPackage(ctx)
+	case "net":
+		err = w.processNetPackage(ctx)
+	case "internal/poll":
+		err = w.processPollPackage(ctx)
+	default:
 		return nil
 	}
 
-	for src, dst := range wasmEdgeSyscallsReplaceFiles {
+	if err != nil {
+		return err
+	}
+
+	slog.Info("package is...", "package", ctx.Package, "args", ctx.Args)
+
+	return nil
+}
+
+func (w *WASMEdgeSyscalls) processSyscallPackage(ctx *ExecContext) error {
+	var replacedFiles = map[string]string{
+		"src/syscall/net_fake.go":   "syscall/net_fake.go.replaced",
+		"src/syscall/net_wasip1.go": "syscall/net_wasip1.go.replaced",
+	}
+
+	for src, dst := range replacedFiles {
 		// Read the file from the embedded filesystem
 		content, err := fs.ReadFile("wasmedgesyscalls/" + dst)
 		if err != nil {
@@ -52,7 +69,97 @@ func (w *WASMEdgeSyscalls) Apply(ctx *ExecContext) error {
 			}
 		}
 	}
-	slog.Info("package is...", "package", ctx.Package, "args", ctx.Args)
+	return nil
+}
+
+func (w *WASMEdgeSyscalls) processNetPackage(ctx *ExecContext) error {
+	var removedFiles = []string{
+		"src/net/net_fake.go",
+		"src/net/fd_fake.go",
+		"src/net/file_wasip1.go",
+		"src/net/fd_wasip1.go",
+		"src/net/sockopt_fake.go",
+	}
+	var addedFiles = []string{
+		"src/net/fd_posix.go",
+		"src/net/fd_unix.go",
+		"src/net/sock_posix.go",
+		"src/net/file_unix.go",
+	}
+	var addedFilesFromFS = []string{
+		"net/fake.go.added",
+		"net/sockopt_wasip1.go.added",
+	}
+
+	var baseDir string
+	for _, src := range removedFiles {
+		for i, arg := range ctx.Args {
+			b, found := strings.CutSuffix(arg, src)
+
+			if !found {
+				continue
+			}
+
+			// Remove the argument
+			ctx.Args = append(ctx.Args[:i], ctx.Args[i+1:]...)
+			slog.Info("Removed argument", "removed", arg)
+
+			baseDir = b
+		}
+	}
+
+	for _, src := range addedFiles {
+		addedPath := filepath.Join(baseDir, src)
+
+		ctx.Args = append(ctx.Args, addedPath)
+		slog.Info("Added argument", "added", addedPath)
+	}
+
+	for _, src := range addedFilesFromFS {
+		// Read the file from the embedded filesystem
+		content, err := fs.ReadFile("wasmedgesyscalls/" + src)
+		if err != nil {
+			slog.Error("Error reading embedded file", "file", src, "error", err)
+			return err
+		}
+
+		// Prepare a temporary file with the content
+		tmpFile, err := w.prepareTmpFile(string(content))
+		if err != nil {
+			slog.Error("Error preparing temp file", "file", src, "error", err)
+			return err
+		}
+
+		ctx.Args = append(ctx.Args, tmpFile)
+		slog.Info("Added argument", "added", tmpFile)
+	}
+
+	return nil
+}
+
+func (w *WASMEdgeSyscalls) processPollPackage(ctx *ExecContext) error {
+	var addedFilesFromFS = []string{
+		"poll/sockopt.go.added",
+	}
+
+	for _, src := range addedFilesFromFS {
+		// Read the file from the embedded filesystem
+		content, err := fs.ReadFile("wasmedgesyscalls/" + src)
+		if err != nil {
+			slog.Error("Error reading embedded file", "file", src, "error", err)
+			return err
+		}
+
+		// Prepare a temporary file with the content
+		tmpFile, err := w.prepareTmpFile(string(content))
+		if err != nil {
+			slog.Error("Error preparing temp file", "file", src, "error", err)
+			return err
+		}
+
+		ctx.Args = append(ctx.Args, tmpFile)
+		slog.Info("Added argument", "added", tmpFile)
+	}
 
 	return nil
 }
--- testdata/httpget/main.go
+++ testdata/httpget/main.go
@@ -2,27 +2,29 @@ package main
 
 import (
 	"io"
+	"log"
 	"net"
 	"net/http"
 	"os"
+	"time"
 )
 
+func init() {
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+	http.DefaultTransport.(*http.Transport).DialContext = dialer.DialContext
+}
+
 func main() {
-	dialer := net.Dialer{}
-
-	tr := http.Transport{
-		DialContext: dialer.DialContext,
-	}
-
-	client := http.Client{
-		Transport: &tr,
-	}
-
-	resp, err := client.Get("http://example.com")
+	resp, err := http.Get("http://httpbin.org/get")
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
+
+	log.Println("Response Status:", resp.Status)
 	if resp.StatusCode != http.StatusOK {
 		panic("failed to get a successful response")
 	}
